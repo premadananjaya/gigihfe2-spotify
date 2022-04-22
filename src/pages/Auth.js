a39d08091daca251bcb66667c7e50efd28a454c9
premadananjaya <premadananjaya@gmail.com>
2022-04-22 14:00:48 +0800

    New Update for Production Deployment, README, and more

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -46,7 +46,7 @@ export default function Auth() {
     return 'https://accounts.spotify.com/authorize?' +
       `client_id=${clientId}` +
       '&response_type=token' +
-      '&redirect_uri=https://sepotivi.vercel.app/' +
+      '&redirect_uri=sepotivi.vercel.app' +
       `&state=${state}` +
       `&scope=${config.SPOTIFY_SCOPE}`;
   }
